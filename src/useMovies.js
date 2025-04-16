5c270f9065f6c90ca4c54ce471bda8079cc363aa
Xtract01 <meena.balwir149@gmail.com>
2025-04-17 01:49:27 +0530

    first commit

---
+++ src/useMovies.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+const KEY = "ee4c9b92";
+export function useMovies(query, callback) {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  useEffect(
+    function () {
+      const controller = new AbortController();
+      async function fetchMovies() {
+        try {
+          setIsLoading(true);
+          setError("");
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+          );
+          if (!res.ok)
+            throw new Error("Something went wrong with fetching movies");
+
+          const data = await res.json();
+          if (data.Response === "False") throw new Error("Movie not found");
+          if (data) {
+            setMovies(data.Search);
+            setError("");
+          }
+
+          setIsLoading(false);
+        } catch (err) {
+          console.error(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
+          setError(err.message);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+      fetchMovies();
+      return function () {
+        controller.abort();
+      };
+    },
+    [query]
+  );
+  return { movies, isLoading, error };
+}
